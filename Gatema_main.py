3c2bc70906134f69302dde9e17311e36e2838fd4
Ex4m <121984532+Ex4m@users.noreply.github.com>
2023-03-14 00:06:28 +0100

    Division into sections

--- Gatema_main.py
+++ Gatema_main.py
@@ -1,5 +1,31 @@
+import re
 
-
-
-with open() as f:
+with open("D327971_fc1.i") as f:
+    content = f.read()
     
+    
+def find_data(content):
+    pattern = r"X.*?T0\d+\n"
+    matches = re.findall(pattern, content)
+    sections = []
+    for i in range(len(matches)):
+        start_index = content.find(matches[i])
+        if i < len(matches)-1:
+            end_index = content.find(matches[i+1])
+        else:
+            end_index = len(content)
+        section = content[start_index:end_index]
+        sections.append(section.strip())
+    return sections    
+        
+    
+    
+    
+  
+    
+    
+    
+sections = find_data(content)    
+    
+print(sections[0])
+#print(repr(content))
